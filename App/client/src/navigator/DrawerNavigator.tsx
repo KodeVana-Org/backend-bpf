f904b828589299c5a2ddfad32c8c4505503e9f6a
Sahisway <sahilswargiary.personal@gmail.com>
2024-02-29 14:19:22 +0530

    Add navigator files

--- App/client/src/navigator/DrawerNavigator.tsx
+++ App/client/src/navigator/DrawerNavigator.tsx
@@ -1,53 +1,54 @@
 import React from 'react';
 import {
-  BottomTabBarProps,
-  createBottomTabNavigator,
-} from '@react-navigation/bottom-tabs';
-import CustomBottomTab from '../components/BottomButton/CustomBottomTab';
-// import HomeScreen from '../screens/HomeScreen';
-// import PostScreen from '../screens/PostScreen';
-// import VideoScreen from '../screens/VideoScreen';
-// import LiveScreen from '../screens/LiveScreen';
-// import ConferenceScreen from '../screens/ConferenceScreen';
+  DrawerScreenProps,
+  createDrawerNavigator,
+} from '@react-navigation/drawer';
+import CustomSideDrawer from '../components/Drawer/CustomSideDrawer.tsx';
+import BTCHistoryScreen from '../screens/drawer/BTCHistoryScreen.tsx';
+import MissionScreen from '../screens/drawer/MissionScreen.tsx';
+import AchievementScreen from '../screens/drawer/AchievementScreen.tsx';
+import VissionScreen from '../screens/drawer/VissionScreen.tsx';
+import CommitteeScreen from '../screens/drawer/CommitteeScreen.tsx';
+import GalleryScreen from '../screens/drawer/GalleryScreen.tsx';
+import AboutScreen from '../screens/drawer/AboutScreen.tsx';
 
-import BTCHistoryScreen from '../screens/BTCHistoryScreen';
-import MissionScreen from '../screens/MissionScreen';
-import AchievementScreen from '../screens/AchievementScreen';
-import VissionScreen from '../screens/VissionScreen.tsx';
-import CommitteeScreen from '../screens/VissionScreen.tsx';
-import MessageScreen from '../screens/MissionScreen.tsx';
-import GalleryScreen from '../screens/BTCHistoryScreen.tsx';
-import AboutScreen from '../screens/AchievementScreen.tsx';
-
-export type BottomTabParamList = {
-  Home: undefined;
-  Post: undefined;
-  Video: undefined;
-  Live: undefined;
-  Conference: undefined;
+export type DrawerParamList = {
+  navigate(arg0: string): void;
+  History: undefined;
+  Mission: undefined;
+  Achievement: undefined;
+  Vission: undefined;
+  Committee: undefined;
+  Gallery: undefined;
+  About: undefined;
 };
 
-const CustomBottomTabs = (props: BottomTabBarProps) => {
-  return <CustomBottomTab {...props} />;
-};
-
-const BottomTabNavigator = () => {
-  const Tab = createBottomTabNavigator<BottomTabParamList>();
-
+const CustomSideDrawers = () => {
   return (
-    <Tab.Navigator
-      tabBar={CustomBottomTabs}
-      screenOptions={{headerShown: false}}>
-      <Tab.Screen name="Home" component={BTCHistoryScreen} />
-      <Tab.Screen name="Post" component={MissionScreen} />
-      <Tab.Screen name="Video" component={AchievementScreen} />
-      <Tab.Screen name="Live" component={VissionScreen} />
-      <Tab.Screen name="Conference" component={CommitteeScreen} />
-      <Tab.Screen name="Video" component={MessageScreen} />
-      <Tab.Screen name="Live" component={GalleryScreen} />
-      <Tab.Screen name="Conference" component={AboutScreen} />
-    </Tab.Navigator>
+    <CustomSideDrawer
+      active={undefined}
+      translateX={undefined}
+      drawerWidth={undefined}
+    />
   );
 };
 
-export default BottomTabNavigator;
+const DrawerNavigator = () => {
+  const Stack = createDrawerNavigator<DrawerParamList>();
+
+  return (
+    <Stack.Navigator
+      drawerContent={CustomSideDrawers}
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="History" component={BTCHistoryScreen} />
+      <Stack.Screen name="Mission" component={MissionScreen} />
+      <Stack.Screen name="Achievement" component={AchievementScreen} />
+      <Stack.Screen name="Vission" component={VissionScreen} />
+      <Stack.Screen name="Committee" component={CommitteeScreen} />
+      <Stack.Screen name="Gallery" component={GalleryScreen} />
+      <Stack.Screen name="About" component={AboutScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default DrawerNavigator;
